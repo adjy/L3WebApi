21ebdf1080149dae0492dfe6628b762e4ab8ba4a
Adjy Sedar Desir <desiradjysedar@gmail.com>
2024-05-06 13:26:11 +0200

    Update Program.cs

--- L3WebApi.WebAPI/Program.cs
+++ L3WebApi.WebAPI/Program.cs
@@ -5,44 +5,51 @@ using L3WebApi.DataAccess.Interfaces;
 using NLog;
 using NLog.Web;
 
-var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
-logger.Info("Starting Application ...");
+namespace L3WebApi.WebAPI;
 
-try {
-	var builder = WebApplication.CreateBuilder(args);
+public class Program {
+	public static void Main(string[] args) {
 
-	// Add services to the container.
-	builder.Services.AddTransient<IGamesDataAccess, GamesDataAccess>();
-	builder.Services.AddTransient<IGameService, GameService>();
+		var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+		logger.Info("Starting Application ...");
 
-	builder.Services.AddControllers();
-	// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-	builder.Services.AddEndpointsApiExplorer();
-	builder.Services.AddSwaggerGen();
+		try {
+			var builder = WebApplication.CreateBuilder(args);
 
-	// NLog: Setup NLog for Dependency injection
-	builder.Logging.ClearProviders();
-	builder.Host.UseNLog();
+			// Add services to the container.
+			builder.Services.AddTransient<IGamesDataAccess, GamesDataAccess>();
+			builder.Services.AddTransient<IGameService, GameService>();
 
-	var app = builder.Build();
+			builder.Services.AddControllers();
+			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+			builder.Services.AddEndpointsApiExplorer();
+			builder.Services.AddSwaggerGen();
 
-	// Configure the HTTP request pipeline.
-	if (app.Environment.IsDevelopment()) {
-		app.UseSwagger();
-		app.UseSwaggerUI();
+			// NLog: Setup NLog for Dependency injection
+			builder.Logging.ClearProviders();
+			builder.Host.UseNLog();
+
+			var app = builder.Build();
+
+			// Configure the HTTP request pipeline.
+			if (app.Environment.IsDevelopment()) {
+				app.UseSwagger();
+				app.UseSwaggerUI();
+			}
+
+			app.UseHttpsRedirection();
+
+			app.UseAuthorization();
+
+			app.MapControllers();
+
+			app.Run();
+		} catch (Exception e) {
+			logger.Error(e);
+			throw;
+		} finally {
+			// Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+			NLog.LogManager.Shutdown();
+		}
 	}
-
-	app.UseHttpsRedirection();
-
-	app.UseAuthorization();
-
-	app.MapControllers();
-
-	app.Run();
-} catch (Exception e) {
-	logger.Error(e);
-	throw;
-} finally {
-	// Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
-	NLog.LogManager.Shutdown();
 }
