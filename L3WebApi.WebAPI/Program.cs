6b1a9992d85b77c86019470623409404a607f463
Adjy Sedar Desir <desiradjysedar@gmail.com>
2024-05-06 11:13:58 +0200

    Update Program.cs

--- L3WebApi.WebAPI/Program.cs
+++ L3WebApi.WebAPI/Program.cs
@@ -2,29 +2,47 @@ using L3WebApi.Business.Implementations;
 using L3WebApi.Business.Interfaces;
 using L3WebApi.DataAccess.Implementations;
 using L3WebApi.DataAccess.Interfaces;
+using NLog;
+using NLog.Web;
 
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddTransient<IGamesDataAccess, GamesDataAccess>();
-builder.Services.AddTransient<IGameService, GameService>();
-// Add services to the container.
+var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+logger.Info("Starting Application ...");
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+try {
+	var builder = WebApplication.CreateBuilder(args);
 
-var app = builder.Build();
+	// Add services to the container.
+	builder.Services.AddTransient<IGamesDataAccess, GamesDataAccess>();
+	builder.Services.AddTransient<IGameService, GameService>();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment()) {
-	app.UseSwagger();
-	app.UseSwaggerUI();
+	builder.Services.AddControllers();
+	// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+	builder.Services.AddEndpointsApiExplorer();
+	builder.Services.AddSwaggerGen();
+
+	// NLog: Setup NLog for Dependency injection
+	builder.Logging.ClearProviders();
+	builder.Host.UseNLog();
+
+	var app = builder.Build();
+
+	// Configure the HTTP request pipeline.
+	if (app.Environment.IsDevelopment()) {
+		app.UseSwagger();
+		app.UseSwaggerUI();
+	}
+
+	app.UseHttpsRedirection();
+
+	app.UseAuthorization();
+
+	app.MapControllers();
+
+	app.Run();
+} catch (Exception e) {
+	logger.Error(e);
+	throw;
+} finally {
+	// Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+	NLog.LogManager.Shutdown();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
