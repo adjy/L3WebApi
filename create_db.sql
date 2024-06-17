7184a4c4d07e383f5460d63b809b9ef5a87f78e8
Adjy Sedar Desir <desiradjysedar@gmail.com>
2024-06-17 18:56:34 +0200

    Update create_db.sql

--- create_db.sql
+++ create_db.sql
@@ -5,3 +5,6 @@ CREATE DATABASE l3webapi
     LOCALE_PROVIDER = 'libc'
     CONNECTION LIMIT = -1
     IS_TEMPLATE = False;
+
+INSERT INTO games (name, description, logo) VALUES
+('Zelda', 'Description 1 vvoainvvzz zvz vz', 'Logo1.png');
