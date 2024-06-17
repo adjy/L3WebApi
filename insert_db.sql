b931e4235800fb64a42a1d672926f76a997bcf27
Adjy Sedar Desir <desiradjysedar@gmail.com>
2024-06-17 19:26:06 +0200

    Update insert_db.sql

--- insert_db.sql
+++ insert_db.sql
@@ -1,1 +1,9 @@
+CREATE TABLE games (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    logo VARCHAR(255) NOT NULL
+);
 
+INSERT INTO games (id, name, description, logo) VALUES
+(1, 'Zelda', 'Zelda le fameux jeu', 'logo Zelda');
