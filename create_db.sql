3ee643331a9e488d52e7e2fe765d4f4274047525
Adjy Sedar Desir <desiradjysedar@gmail.com>
2024-06-17 19:25:51 +0200

    Update create_db.sql

--- create_db.sql
+++ create_db.sql
@@ -6,12 +6,4 @@ CREATE DATABASE l3webapi
     CONNECTION LIMIT = -1
     IS_TEMPLATE = False;
 
-CREATE TABLE games (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    description TEXT NOT NULL,
-    logo VARCHAR(255) NOT NULL
-);
 
-INSERT INTO games (id, name, description, logo) VALUES
-(1, 'Zelda', 'Zelda le fameux jeu', 'logo Zelda');
